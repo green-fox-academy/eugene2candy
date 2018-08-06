a2ba33fe1d83e1345b637534c7381e4922ba9d41
eugene2candy <105043487@qq.com>
2018-08-06 15:38:41 +0800

    all except list manager

--- week-03/day-1/list-manager/main.js
+++ week-03/day-1/list-manager/main.js
@@ -1,2 +1,25 @@
 'use-strict';
 
+const list = ['bread', 'milk', 'orange', 'tomato'];
+const ulleft = document.createElement('col');
+const ulright = document.createElement('col');
+const ulbuttons = document.createElement('col');
+const trow = document.createElement('tr');
+const table = document.createElement('table');
+document.body.appendChild(table);
+table.appendChild(trow);
+trow.appendChild(ulleft);
+trow.appendChild(ulbuttons);
+trow.appendChild(ulright);
+
+for (let i = 0; i < list.length; i++) {
+  const td = document.createElement('td');
+  td.textContent = list[i];
+  ulleft.appendChild(td);
+}
+
+const up = document.createElement('button');
+const bigger = document.createElement('button');
+const x = document.createElement('button');
+const down = document.createElement('button');
+
---
+++ week-03/day-1/svg-1/svg-1.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+const rec = document.querySelector('#very_rectangle');
+// console.log(rec);
+rec.setAttribute('x', 50);
+rec.setAttribute('y', 50);
+rec.style.fill = 'tomato';
