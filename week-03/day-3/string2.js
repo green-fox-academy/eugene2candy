a44070c54448ae5d7563e137555539daeeff40a1
eugene2candy <105043487@qq.com>
2018-08-08 11:17:17 +0800

    ex8

--- week-03/day-3/string2.js
+++ week-03/day-3/string2.js
@@ -1,3 +1,18 @@
 'use strict';
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+let result = [];
+const repl = (param) => {
+  if (param.length > 0) {
+    if (param.slice(0, 1) === 'x') {
+      result.push('');
+      repl(param.slice(1, param.length));
+    } else {
+      result.push(param.slice(0, 1));
+      repl(param.slice(1, param.length));
+    }
+  } else {
+    console.log(result.join(''));
+  }
+};
 
+repl('xyx');
