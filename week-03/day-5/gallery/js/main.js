6d1b3ac57592ba53f454f33e88abab09def7eebd
eugene2candy <105043487@qq.com>
2018-08-10 17:13:07 +0800

    second update again

--- week-03/day-5/gallery/js/main.js
+++ week-03/day-5/gallery/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
+const x = document.getElementsByClassName('slide');
 
 let slideIndex = 3;
 
 function displayIMG(n) {
-  const x = document.getElementsByClassName('slide');
   if (n > x.length) {
     slideIndex = 1;
   }
@@ -16,17 +16,15 @@ function displayIMG(n) {
   x[slideIndex - 1].style.display = 'block';
 }
 
+displayIMG(slideIndex);
+
 const last = () => {
-  displayIMG(slideIndex += -1);
+  displayIMG(slideIndex -= 1);
 };
 
 const next = () => {
   displayIMG(slideIndex += 1);
 };
-
-function currentIMG(n) {
-  displayIMG(slideIndex = n);
-}
 
 const btnleft = document.getElementById('b1');
 const btnright = document.getElementById('b2');
@@ -35,7 +33,6 @@ btnleft.addEventListener('click', last);
 btnright.addEventListener('click', next);
 
 document.getElementById('buttonclick').addEventListener('click', (event) => {
-  currentIMG(event.target.alt);
+  slideIndex = event.target.alt;
+  displayIMG(slideIndex);
 });
-
-displayIMG(slideIndex);
